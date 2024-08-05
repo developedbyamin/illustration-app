9fa3568253a629ffa7aa2f827d5ab5f974be901b
developedbyamin <developedbyamin@gmail.com>
2024-08-05 17:35:01 +0400

    task done

--- lib/presentation/overview/state/favorite_state.dart
+++ lib/presentation/overview/state/favorite_state.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:interntask/domain/usecases/toggle_favorite_status.dart';
 
 class FavoriteState extends ChangeNotifier {
   bool _isFavorite = false;
 
   bool get isFavorite => _isFavorite;
 
+  ToggleFavoriteStatus favoriteStatus = ToggleFavoriteStatus();
   void toggleFavorite() {
-    _isFavorite = !_isFavorite;
+    _isFavorite = favoriteStatus.execute(_isFavorite);
     notifyListeners();
   }
 }
